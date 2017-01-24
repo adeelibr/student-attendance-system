21d9aff461cea55c9736b3d6614406724887d875
adeelibr <adeelimranr@gmail.com>
2017-01-25 00:10:18 +0500

    Added morgan logger

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
+const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -8,6 +9,9 @@ const app = express();
 // Parsers for POST data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// Logger
+app.use(morgan('dev'));
 
 // Point static path to dist
 app.use(express.static(path.join(__dirname, 'dist')));
@@ -30,4 +34,4 @@ app.set('port', port);
 const server = http.createServer(app);
 
 /** Listen on provided port, on all network interfaces. */
-server.listen(port, () => console.log(`API running on localhost:${port}`))
+server.listen(port, () => console.log(`Server Running on http://localhost:${port}/`))
